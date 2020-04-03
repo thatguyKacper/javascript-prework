8ff29effce4a19f6df4fdcf92e5796804028b4aa
Kacper Wójtowicz <kacper.m.wojtowicz@gmial.com>
2020-04-03 22:09:07 +0000

    add playerMove and print

--- js/script.js
+++ js/script.js
@@ -2,3 +2,4 @@ let computerMove = 'kamień';
 let playerMove = 'papier';
 
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Twój ruch to ' + playerMove + '! Wygrałeś!');
