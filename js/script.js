dea58a4a499b577d9a0f0b7dd777e2ba249fe0f9
Kacper Wójtowicz <kacper.m.wojtowicz@gmial.com>
2020-04-07 17:52:55 +0000

    add computerMove

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-let computerMove = 'kamień';
-let playerMove = 'papier';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Twój ruch to ' + playerMove + '! Wygrałeś!');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+else if (randomNumber== 2) {
+  computerMove = 'papier';
+}
+else {
+  computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
