b5a7bdeb63fb9db8ef6f19b6fd2abdb1b4eded02
Kacper Wójtowicz <kacper.m.wojtowicz@gmial.com>
2020-04-09 19:35:58 +0000

    version 1.1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,52 @@
+function getMoveName(argMoveId){
+  if(argMoveId == 1)  {
+    return 'kamień';
+  }
+  else if (argMoveId == 2) {
+    return 'papier';
+  }
+  else if (argMoveId == 3){
+    return 'nożyce';
+  }
+
+  printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+  return 'nieznany ruch';
+}
+
+function displayResult(argComputerMove, argPlayerMove){
+
+  if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+    return 'score';
+  }
+  else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
+    printMessage('Ty wygrywasz!');
+    return 'score';
+  }
+  else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
+    printMessage('Ty wygrywasz!');
+    return 'score';
+  }
+  else if (argComputerMove == argPlayerMove) {
+    printMessage('Remis!');
+    return 'score';
+  }
+  else if (playerMove == 'nieznany ruch') {
+    printMessage('Nieznany ruch!');
+  }
+  else {
+    printMessage('Ja wygrywam!');
+    return 'score';
+  }
+}
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+let argComputerMove = getMoveName(randomNumber);
 
+/*
 if(randomNumber == 1){
   computerMove = 'kamień';
 }
@@ -13,13 +56,14 @@ else if (randomNumber == 2) {
 else {
   computerMove = 'nożyce';
 }
-
+*/
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let argPlayerMove = getMoveName(playerInput);
 
+/*
 if(playerInput == '1'){
   playerMove = 'kamień';
 }
@@ -29,6 +73,7 @@ else if (playerInput== '2') {
 else {
   playerInput = 'nożyce';
 }
+
 
 printMessage('Twój ruch to: ' + playerMove);
 
@@ -50,3 +95,6 @@ else if (computerMove == playerMove) {
 else {
   printMessage('Ja wygrywam!');
 }
+*/
+
+let score = displayResult(argComputerMove, argPlayerMove);
